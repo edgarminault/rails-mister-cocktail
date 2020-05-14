548929fc21fad0cd04a012a30caa91fb2db7765a
Edgar Minault <e.minault@bluewin.ch>
2020-05-14 13:50:17 +0200

    Read all fct done. Grid view implemented. Logo in navbar inserted. Images in cards implemented.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
+require 'faker'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-drinks_serialized = open(url).read
-drinks = JSON.parse(drinks_serialized)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# drinks_serialized = open(url).read
+# drinks = JSON.parse(drinks_serialized)
 
-puts 'Creating ingredients from the JSON.'
+# puts 'Creating ingredients from the JSON.'
 
-drinks["drinks"].each do |element|
-  ingredient = Ingredient.new(name: element["strIngredient1"])
-  ingredient.save!
-  puts "Ingredient added."
+# drinks["drinks"].each do |element|
+#   ingredient = Ingredient.new(name: element["strIngredient1"])
+#   ingredient.save!
+#   puts "Ingredient added."
+# end
+
+# puts 'All ingredients added!'
+
+puts 'Creating 20 fake virgin cocktails...'
+20.times do
+  cocktail = Cocktail.new(
+    name: Faker::DcComics.villain
+  )
+  cocktail.save!
+  puts 'Cocktail added.'
 end
-
-puts 'All ingredients added!'
+puts 'All cocktails added.'
 
