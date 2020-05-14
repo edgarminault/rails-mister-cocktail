47b6dbc216a95e1a4c73569d388bb6934826eb98
Edgar Minault <e.minault@bluewin.ch>
2020-05-14 14:19:21 +0200

    Read one fct done. Nice layout with banner, title and fontawesome icons.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,4 +2,26 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new # needed to instantiate the form_for
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
