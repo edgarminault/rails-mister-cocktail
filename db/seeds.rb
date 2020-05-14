0beaa1fb777ad98caf0079c75c4afdcca83a67d5
Edgar Minault <e.minault@bluewin.ch>
2020-05-14 12:41:36 +0200

    JSON parsed and inserted in seed. Ingredients seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+drinks = JSON.parse(drinks_serialized)
+
+puts 'Creating ingredients from the JSON.'
+
+drinks["drinks"].each do |element|
+  ingredient = Ingredient.new(name: element["strIngredient1"])
+  ingredient.save!
+  puts "Ingredient added."
+end
+
+puts 'All ingredients added!'
+
